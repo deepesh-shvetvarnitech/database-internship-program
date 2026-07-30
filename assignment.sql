c67d53363fd0d37c195b14e5d0d8d107653663ad
deepesh-shvetvarnitech <deepeshchouhan.shvetvarnitech@gmail.com>
2026-07-30 15:27:18 +0530

    Add files via upload

---
+++ assignment.sql
@@ -0,0 +1,11 @@
+CREATE TABLE Student (
+    id INT PRIMARY KEY,
+    Name VARCHAR(50),
+    Age INT
+);
+INSERT INTO Student
+VALUES (1, 'Rahul', 18);
+
+INSERT INTO Student
+VALUES (2, 'Priya', 19);
+SELECT * FROM Student;
